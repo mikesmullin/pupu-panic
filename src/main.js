445ce232d0f528a50d89794791c968299e759dab
Paul Milham <domreinalabaster@gmail.com>
2015-01-23 22:18:35 -0700

    Initial commit with dave code as place holder
    Signed-off-by: Paul Milham <domreinalabaster@gmail.com>

---
+++ src/main.js
@@ -0,0 +1,8 @@
+var game = new Phaser.Game(720, 1100, Phaser.AUTO, "");
+game.state.add("Boot", Boot);
+game.state.add("Preload", Preload);
+game.state.add("Title", Title);
+game.state.add("Play", Play);
+game.state.start("Boot");
+
+var highScore = 0;
