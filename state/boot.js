76c99ab67fd07609605ee17c55c5edbae4dbca76
Mike Smullin <mike@smullindesign.com>
2015-01-25 12:45:54 -0700

    faster preload

--- state/boot.js
+++ state/boot.js
@@ -5,6 +5,7 @@ var Boot = {
     game.scale.pageAlignHorizontally = true;
 
     game.load.image("PreloadBar", "assets/PreloadBar.png");
+    game.load.image('Loading', 'assets/loading.png');
   },
   create: function() {
     game.input.maxPointers = 1;
