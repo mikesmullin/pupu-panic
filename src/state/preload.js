ba3fbeacabcbb8dd73d53e3fa79d970bb4f88c24
Mike Smullin <mike@smullindesign.com>
2015-01-31 13:58:49 -0700

    switch to new audio formats; dont use mp3 it sucks

--- src/state/preload.js
+++ src/state/preload.js
@@ -10,25 +10,25 @@ var Preload = {
 
     game.load.image('mess', 'assets/mess.png');
 
-    game.load.audio("TitleMusic", ["assets/Title_Music_mix.mp3", "assets/Title_Music_mix.ogg"]);
-    game.load.audio("LevelMusicIntro", ["assets/lvl_Intro_1.mp3", "assets/lvl_Intro_1.ogg"]);
-    game.load.audio("LevelMusicLoop", ["assets/lvl_Loop_1.mp3", "assets/lvl_Loop_1.ogg"]);
+    game.load.audio("TitleMusic", ["assets/Title_Music_mix.ogg", "assets/Title_Music_mix.m4a"]);
+    game.load.audio("LevelMusicIntro", ["assets/lvl_Intro_1.ogg", "assets/lvl_Intro_1.m4a"]);
+    game.load.audio("LevelMusicLoop", ["assets/lvl_Loop_1.ogg", "assets/lvl_Loop_1.m4a"]);
 
-    game.load.audio("ButtonClick", ["assets/Button_Click_1.mp3", "assets/Button_Click_1.ogg"]);
-    game.load.audio("EatingFood", ["assets/Eating_Food_1.mp3", "assets/Eating_Food_1.ogg"]);
-    game.load.audio("FindingPotty", ["assets/Finding_Potty.mp3", "assets/Finding_Potty.ogg"]);
-    game.load.audio("Janitor", ["assets/Janitor.mp3", "assets/Janitor.ogg"]);
-    game.load.audio("MessSound", ["assets/Mess_Sound.mp3", "assets/Mess_Sound.ogg"]);
-    game.load.audio("MoneyGained", ["assets/Money_Gained_1.mp3", "assets/Money_Gained_1.ogg"]);
-    game.load.audio("MoneyLost", ["assets/MoneyLost_1.mp3", "assets/MoneyLost_1.ogg"]);
-    game.load.audio("PickingUpFood", ["assets/Picking_up_food_1.mp3", "assets/Picking_up_food_1.ogg"]);
-    game.load.audio("PortaPottyOpen", ["assets/Porta_Potty_Open_1.mp3", "assets/Porta_Potty_Open_1.ogg"]);
-    game.load.audio("ScrollBarClick", ["assets/Scroll_bar_Click_1.mp3", "assets/Scroll_bar_Click_1.ogg"]);
-    game.load.audio("UhOh", ["assets/uh_oh_1.mp3", "assets/uh_oh_1.ogg"]);
-    game.load.audio("YouWin", ["assets/You_Win_1.mp3", "assets/You_Win_1.ogg"]);
-    game.load.audio("YouLose", ["assets/Lose_1.mp3", "assets/Lose_1.ogg"]);
+    game.load.audio("ButtonClick", ["assets/Button_Click_1.ogg", "assets/Button_Click_1.m4a"]);
+    game.load.audio("EatingFood", ["assets/Eating_Food_1.ogg", "assets/Eating_Food_1.m4a"]);
+    game.load.audio("FindingPotty", ["assets/Finding_Potty.ogg", "assets/Finding_Potty.m4a"]);
+    game.load.audio("Janitor", ["assets/Janitor.ogg", "assets/Janitor.m4a"]);
+    game.load.audio("MessSound", ["assets/Mess_Sound.ogg", "assets/Mess_Sound.m4a"]);
+    game.load.audio("MoneyGained", ["assets/Money_Gained_1.ogg", "assets/Money_Gained_1.m4a"]);
+    game.load.audio("MoneyLost", ["assets/MoneyLost_1.ogg", "assets/MoneyLost_1.m4a"]);
+    game.load.audio("PickingUpFood", ["assets/Picking_up_food_1.ogg", "assets/Picking_up_food_1.m4a"]);
+    game.load.audio("PortaPottyOpen", ["assets/Porta_Potty_Open_1.ogg", "assets/Porta_Potty_Open_1.m4a"]);
+    game.load.audio("ScrollBarClick", ["assets/Scroll_bar_Click_1.ogg", "assets/Scroll_bar_Click_1.m4a"]);
+    game.load.audio("UhOh", ["assets/uh_oh_1.ogg", "assets/uh_oh_1.m4a"]);
+    game.load.audio("YouWin", ["assets/You_Win_1.ogg", "assets/You_Win_1.m4a"]);
+    game.load.audio("YouLose", ["assets/Lose_1.ogg", "assets/Lose_1.m4a"]);
 
-    game.load.audio("Gummibar", ["assets/gummibar_funny_dj.mp3", "assets/gummibar_funny_dj.ogg"]);
+    game.load.audio("Gummibar", ["assets/gummibar_funny_dj.ogg", "assets/gummibar_funny_dj.m4a"]);
   },
   create: function() {
     game.globals.playTitleMusic();
