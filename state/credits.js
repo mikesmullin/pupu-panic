5a45b62ddcd26eece0933aed52d375c003842efa
Mike Smullin <mike@smullindesign.com>
2015-01-31 18:27:58 -0700

    publish

--- state/credits.js
+++ state/credits.js
@@ -3,26 +3,25 @@ var backButton,
 
 var Credits = {
   preload: function() {
-    game.load.image('credits', 'assets/credits.png');
   },
   create: function() {
     var _this = this;
-    game.stage.backgroundColor = '#0aa'
+    game.stage.backgroundColor = '#558934'
+    credits = this.game.add.sprite(game.world.centerX, game.world.centerY - 90, 'Credits');
+    credits.anchor.setTo(.5,.5);
 
-    backButton = this.game.add.sprite(game.width / 2, game.height - 120, "Sprites", "Food_CarrotBun_Good_1.png");
-    backButton.anchor.setTo(0.5, 0.5);
+    backButton = this.game.add.sprite(game.world.centerX, game.height - 100, "Sprites", "Food_CarrotBun_Good_1.png");
+    backButton.anchor.setTo(.5, .5);
     backButton.inputEnabled = true;
     backButton.events.onInputDown.add(function() {
       game.state.start("Title");
     });
 
-    credits = this.game.add.sprite(50, 50, 'credits');
-    credits.scale.setTo(3,3);
   },
   update: function() {
   },
   render: function() {
-    game.debug.text('back', backButton.x-30, backButton.y+30, '#000000', '80px arial');
+    game.debug.text('back', backButton.x-30, backButton.y+30, '#000000', '80px Bangers');
   },
   shutdown: function() {
   }
